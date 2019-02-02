a8f21217c38ab9ecce28c178dbc640b94af9a62c
Bryan Dickens <bigbrd1026@gmail.com>
2019-02-01 16:02:11 -0800

    Initial Commit

---
+++ ProductivityTrackerTests/ProductivityTrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProductivityTrackerTests.swift
+//  ProductivityTrackerTests
+//
+//  Created by Bryan Dickens on 2/1/19.
+//  Copyright © 2019 Bryan Dickens. All rights reserved.
+//
+
+import XCTest
+@testable import ProductivityTracker
+
+class ProductivityTrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
